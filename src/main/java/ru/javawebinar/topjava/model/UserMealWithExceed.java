726b54ca7616fd814c9ca44f13a26d8885c50bf5
Sanzhar <sanjar.satybaldiyev@gmail.com>
2016-08-29 22:01:03 +0500

    implemented getFilteredWithExceeded method

---
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
@@ -0,0 +1,29 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDateTime;
+
+/**
+ * GKislin
+ * 11.01.2015.
+ */
+public class UserMealWithExceed {
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
+
+    private final boolean exceed;
+
+    public UserMealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.exceed = exceed;
+    }
+
+//    @Override
+//    public String toString() {
+//        return String.format("desription: %s, date: %s, calories: %d, exceed: %b", description, dateTime, calories, exceed);
+//    }
+}
